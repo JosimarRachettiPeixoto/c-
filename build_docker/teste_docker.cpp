fc9ac75a6901622aa0331f337e9a254fab16e2c0
josimar.rachetti.peixoto@gmail.com <josimarrachetti@gmail.com>
2024-03-25 19:04:35 -0300

    feat: adicionando docker em c++

---
+++ build_docker/teste_docker.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// #include <pqxx/pqxx> 
+
+int main(){
+   
+    cout << "esse é um teste de docker com c++"<< endl;
+
+    exit(0);
+}
